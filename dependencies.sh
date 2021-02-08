deba94ce53ef7944d4a5bc8d0afd723ab0b8eb75
AlejandroMarinP <alpermar@bhdyn.es>
2021-02-08 14:22:07 +0100

    PyInstaller

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
-python -m pip install wx
-python -m pip install python-rapidjson
-python -m pip install pyserial
-python -m pip install threading
-python -m pip install pyinstaller
+python3 -m pip install wx
+python3 -m pip install python-rapidjson
+python3 -m pip install pyserial
+python3 -m pip install threading
+python3 -m pip install pyinstaller
