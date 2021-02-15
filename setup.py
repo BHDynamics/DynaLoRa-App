5dce3aa1fcae291a2d950f59761202ef4e5e65e7
AlejandroMarinP <alpermar@bhdyn.es>
2021-02-15 11:55:31 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
-from setuptools import setup
+import subprocess
+import sys
 
-setup(
-    name='DynaLoRa-App',
-    version='0.1',
-    description='Desktop App to control DynaLoRa hardware module and its messages',
-    license='GNU GPL',
-    author='BHDynamics',
-    author_email='info@bhdyn.com',
-    url='https://www.bhdynamics.info/',
-    packages=['dongle'],
-    install_requires=['wxPython', 'python-rapidjson', 'pyserial', 'pyinstaller'],
-    scripts=[
-        'run.py'
-    ]
-)
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+with open("dependencies.txt", 'r') as dep:
+    packages = dep.readlines()
+    
+    packages = [x.strip() for x in packages]
+    
+    for p in packages:
+        install(p)
